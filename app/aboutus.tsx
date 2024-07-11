a78325867015f20966ad459c31487ffb5ae8a9a9
Samen1010 <samenhossain1805@gmail.com>
2024-07-11 14:08:05 -0400

    test

--- app/aboutus.tsx
+++ app/aboutus.tsx
@@ -1,13 +1,11 @@
-import Image from "next/image";
 import Navbar from "../components/Navbar.jsx";
 import Footer from "../components/Footer.jsx";
-import Hero from "../components/Hero.jsx";
 
 export default function Home() {
   return (
     <div className="bg-white-900">
       <Navbar/>
-      <Hero />
+      <p>Guppy</p>
       <Footer />
     </div>
   );
